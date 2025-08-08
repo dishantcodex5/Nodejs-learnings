25831b900e472eeee45dd1ffd3d8898a2c530ef0
dishantcodex5 <dishantbhavsar58@gmail.com>
2025-08-08 17:49:19 +0530

    sendHtmlFile.js

--- express/sendHtmlFile.js
+++ express/sendHtmlFile.js
@@ -1,1 +1,17 @@
 
+import express from "express";
+import {PORT} from "./env.js";
+import path from "path"
+const app=express()
+
+app.get("/",(req,res)=>{
+
+  const __filename =new URL (import.meta.url).pathname
+  console.log(__filename);
+  const homepage=path.join(import.meta.dirname,"public","index.html")
+  res.sendFile(homepage);
+
+});
+app.listen(PORT,()=>{
+  console.log("the server is runnin  at : ") 
+});
